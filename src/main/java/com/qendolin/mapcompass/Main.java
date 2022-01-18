6a395192a6b1904c1deada42309fcf7a2ccd2415
Qendolin <32160662+Qendolin@users.noreply.github.com>
2022-01-18 23:05:19 +0100

    Change initializer

--- src/main/java/com/qendolin/mapcompass/Main.java
+++ src/main/java/com/qendolin/mapcompass/Main.java
@@ -1,15 +1,15 @@
 package com.qendolin.mapcompass;
 
 import com.qendolin.mapcompass.config.ConfigManager;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main implements ModInitializer {
+public class Main implements ClientModInitializer {
 	public static final String MODID = "mapcompass";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	public static final Config CONFIG = ConfigManager.createOrLoad(new Config());
 
 	@Override
-	public void onInitialize() {}
+	public void onInitializeClient() {}
 }
